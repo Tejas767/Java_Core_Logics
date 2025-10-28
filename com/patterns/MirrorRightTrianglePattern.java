3ce262ba3c5b087f61b60b37d1b538589a74f3af
Varad Mule <varadmule17@gmail.com>
2025-10-28 22:41:53 +0530

    more pattern programs added

---
+++ com/patterns/MirrorRightTrianglePattern.java
@@ -0,0 +1,23 @@
+package com.programs.patterns;
+
+import java.util.Scanner;
+
+public class MirrorRightTrianglePattern {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+			for (int j = 1; j <= num; j++) {
+				if (j <= num - i) {
+					System.out.print(" ");
+				} else {
+					System.out.print(j);
+				}
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
